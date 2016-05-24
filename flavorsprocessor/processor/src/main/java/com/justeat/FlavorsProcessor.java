8a197ec08988dc8c28a545fb5e0a9b4c94f39f53
Michal Zwierzyk <michal.zwierzyk@just-eat.com>
2016-05-24 01:57:30 +0100

    Register annotation processor
    Generate the META-INF/services/javax.annotation.processing.Processor file automatically through Google's AutoService annotation processor

--- flavorsprocessor/processor/src/main/java/com/justeat/FlavorsProcessor.java
+++ flavorsprocessor/processor/src/main/java/com/justeat/FlavorsProcessor.java
@@ -1,10 +1,13 @@
 package com.justeat;
 
+import com.google.auto.service.AutoService;
+
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import java.util.Set;
 
+@AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 @SupportedAnnotationTypes({
         // Set of full qullified annotation type names
